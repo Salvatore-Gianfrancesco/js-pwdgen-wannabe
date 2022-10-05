d6a871b83515717cc9ae661dacd9afaba2eabb9e
Salvatore-Gianfrancesco <salvatore.gianfrancesco@yahoo.it>
2022-10-05 18:02:43 +0200

    change var name using camelCase

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,5 @@
-const user_name = prompt("Inserisci il tuo nome");
-const user_lastname = prompt("Inserisci il tuo cognome");
-const user_color = prompt("Inserisci il tuo colore preferito");
+const userName = prompt("Inserisci il tuo nome");
+const userLastname = prompt("Inserisci il tuo cognome");
+const userColor = prompt("Inserisci il tuo colore preferito");
 
-document.getElementById("password").innerHTML = `La tua password super sicura è: ${user_name}${user_lastname}${user_color}22`
+document.getElementById("password").innerHTML = `La tua password super sicura è: ${userName}${userLastname}${userColor}22`
